8bd3c4c52505f74db90af87bebf0aac08ba6b7cc
Justin Nguyen <Jnguy129@gmail.com>
2025-08-27 10:01:20 -0500

    Changed game.js to script.js and modified the prompt for users to know what the options are

--- script.js
+++ script.js
@@ -13,6 +13,14 @@ function getComputerChoice(){
     }
 }
 
-// function getHumanChoicee(){
+function getHumanChoice(){
+    return prompt("Enter rock, paper, or scissors: ").toLowerCase;
+}
 
+
+console.log(getHumanChoice());
+
+
+// function getRoundWinner(getComputerChoice, getHumanChoice){
+//     if(getComputerChoice)
 // }
