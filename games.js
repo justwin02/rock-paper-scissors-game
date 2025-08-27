0c34605fbb270bc6eeb140a8451ce0148fbd62dd
Justin Nguyen <Jnguy129@gmail.com>
2025-08-26 20:37:58 -0500

    Function getComputerChoice generates a random number bewteen 0  and 2 then returns computer's choice

---
+++ games.js
@@ -0,0 +1,18 @@
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    switch(randomNumber){
+        case 0:
+            return "rock";
+        case 1: 
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            return "error";
+    }
+}
+
+// function getHumanChoicee(){
+
+// }
